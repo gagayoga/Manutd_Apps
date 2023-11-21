63bcff2cc208464867324b43e418b05781942762
FitraRamadhaniB <fitramadhani13@gmail.com>
2023-11-21 13:43:28 +0700

    day2

---
+++ src/Koneksi/Config.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Koneksi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author Asus
+ */
+public class Config {
+    
+    private static Connection MySQLConfig;
+    
+    public static Connection configDB()throws SQLException{
+        try{
+            String url = "jdbc:mysql://localhost:3306/db_manutd";
+            String user = "root";
+            String pass = "";
+            
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+            MySQLConfig = DriverManager.getConnection(url, user, pass);
+        }catch(SQLException e){
+            System.out.println("Koneksi ke database Gagal " + e.getMessage());
+        }
+        
+        return MySQLConfig;
+    }
+}
